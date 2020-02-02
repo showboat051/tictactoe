f348f3b3b2d179b984abdddb747705568fbc158c
Rashad Davis <rashaddavis@Rashads-MBP.attlocal.net>
2020-02-01 19:49:42 -0600

    added buttons

---
+++ main.js
@@ -0,0 +1,9 @@
+/*----- event listeners -----*/
+document.onclick= handleButtonClick;
+
+
+
+function handleButtonClick () {
+    console.log ('clicked')
+}
+// document.getElementsByClassName('gameButton').addEventListener('click' , handleButtonClick)
