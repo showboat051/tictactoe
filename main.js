503312617089741612af41db828f6a223ec621da
Rashad Davis <rashaddavis@Rashads-MacBook-Pro.local>
2020-02-03 09:49:58 -0600

    added board

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
+/*----- CONSTANTS -----*/
+// const xShow = document.getElementsByClassName('gameButton');
+// const xShow = document.onclick.getElementsByClassName('gameButton').innerHTML = 'X';
+// console.log(xShow)
 /*----- event listeners -----*/
-document.onclick= handleButtonClick;
+// document.onclick = xShow();
+// document.onclick.getElementsByClassName('gameButton').innerHTML = 'X';
+document.onclick = handleButtonClick;
 
-
-
-function handleButtonClick () {
+// handles Player Clicks
+function handleButtonClick (e) {
     console.log ('clicked')
 }
+
+function handleButtonClick() {
+    // var x = document.getElementById("0").innerHTML;
+    document.getElementById("0").innerHTML = 'x';
+    // document.onclick.getElementsByClassName('gameButton').innerHTML = 'X';
+  }
 // document.getElementsByClassName('gameButton').addEventListener('click' , handleButtonClick)
+
+//Initial state of the game
+
+function start () {
+    let Player1 = 0;
+    let Player2 = 0;
+}
+
+
